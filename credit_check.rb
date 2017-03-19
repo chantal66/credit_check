ab8ea9f5c67446f93565940dbdc7d2e71ab04a7f
Chantal Justamond <cjustamm66@me.com>
2017-03-19 16:49:17 -0600

    class addded

--- credit_check.rb
+++ credit_check.rb
@@ -1,4 +1,12 @@
 
+class CreditCheck
+
+  def initialize(card_number)
+    @card_number = card_number
+
+    @valid = false
+
+  end 
 
   
   @card_number = "4929735477250543"
@@ -16,7 +24,7 @@
     end
   end  
 
-  p numbers
+ 
 
   def multiply_by_2
     numbers.map.with_index do |value, index|
@@ -30,8 +38,6 @@
     end
   end    
 
-  p multiply_by_2
-
 
   def greater_than_9_and_adding 
     multiply_by_2.map do |item|
@@ -43,23 +49,30 @@
       end
     end    
   end  
-  p greater_than_9_and_adding
+ 
 
   def total 
     greater_than_9_and_adding.reduce(:+)
   end
 
-  p total
-
-  def valid(total) 
+  def valid
     if total % 10 == 0
       'number is valid'
     else 
       'number is invalid'
     end   
   end 
+end
+#puts valid(total)  
 
-puts valid(total)  
+valid_card_1 = CreditCheck.new('5541808923795240')
+puts valid_card_1.valid
+valid_card_2 = CreditCheck.new('5541801923795240')
+puts valid_card_2.valid
+valid_card_3 = CreditCheck.new('342804633855673') #American Express
+puts valid_card_3.valid
+valid_card_3 = CreditCheck.new('342801633855673') #American Express
+puts valid_card_3.valid
 
 
 
